1e45c7f23c909ee20ef184a51cbad4c0c55c52bc
Bryan Moreno <bryan.moreno@tec.mx>
2022-10-31 07:57:51 -0600

    show room's users on sidebar autoscroll

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,9 +10,33 @@ const $messages = document.getElementById('messages');
 // Templates
 const messageTemplate = document.getElementById('message-template').innerHTML;
 const locationMessageTemplate = document.getElementById('location-message-template').innerHTML;
+const sidebarTemplate = document.getElementById('sidebar-template').innerHTML;
 
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
+
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild;
+
+    // Height of the new message
+    const newMessagesStyles = getComputedStyle($newMessage);
+    const newMessageMargin = parseInt(newMessagesStyles.marginBottom);
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+    // Visible height
+    const visibleHeight = $messages.offsetHeight;
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight;
+
+    // How far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight;
+
+    if (containerHeight - newMessageHeight - 1 <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight;
+    }
+}
 
 socket.on('message', (message) => {
     const html = Mustache.render(messageTemplate, {
@@ -21,6 +45,7 @@ socket.on('message', (message) => {
         createdAt: moment(message.createdAt).format('h:mm a'),
     });
     $messages.insertAdjacentHTML('beforeend', html);
+    autoscroll();
 });
 
 socket.on('locationMessage', (message) => {
@@ -30,6 +55,12 @@ socket.on('locationMessage', (message) => {
         createdAt: moment(message.createdAt).format('h:mm a'),
     });
     $messages.insertAdjacentHTML('beforeend', html);
+    autoscroll();
+});
+
+socket.on('roomData', ({ room, users}) => {
+    const html = Mustache.render(sidebarTemplate, { room, users });
+    document.getElementById('sidebar').innerHTML = html;
 });
 
 $messageForm.addEventListener('submit', (e) => {
